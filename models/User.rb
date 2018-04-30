0022e4883993c44352a136c186039ca9b38d212d
Yoon Jo Han <yuniegu3@gmail.com>
2018-04-29 22:46:23 -0400

    All the functional parts are working, just need to fix the names and test it out a bit more to see if anything is missing. also need to find out how to get emails to be unique(no doubles). But for most part, MVP is done

--- models/User.rb
+++ models/User.rb
@@ -1,3 +1,14 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
 	belongs_to :blog
+	
+	  # work on this shit
+	include BCrypt
 end
+
+
+
+
+
+
---
+++ public/main.js
@@ -0,0 +1,8 @@
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+        
+
+function delayedRedirect(){
+    window.location = "/error"
+}
